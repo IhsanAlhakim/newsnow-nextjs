fb7c5321c794dc25573b2559e57aa056ae68690b
IhsanAlhakim <ihsanalhakim04@gmail.com>
2024-12-15 22:06:27 +0700

    delete logout button component file

--- src/app/dashboard/(home)/components/LogoutButton.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { deleteSession } from "../../(auth)/login/lib/session";
-
-export default function LogoutButton() {
-  async function handleLogout() {
-    const logout = await deleteSession();
-  }
-  return <button onClick={handleLogout}>Logout</button>;
-}
